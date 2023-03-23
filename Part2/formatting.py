6ae4f6118c4879290e2551dec4b54199456594c0
BenjaminMJ <rtx245@alumni.ku.dk>
2023-03-23 11:00:38 +0100

    labels to type

--- Part2/formatting.py
+++ Part2/formatting.py
@@ -8,11 +8,11 @@ import nltk
 import re
 from collections import defaultdict
 import math
-import re 
+import re
 import os
 
 from sklearn.decomposition import IncrementalPCA    # inital reduction
-from sklearn.manifold import TSNE  
+from sklearn.manifold import TSNE
 
 from gensim.models import Word2Vec
 from gensim.models import KeyedVectors
@@ -47,22 +47,22 @@ def format(fullCorpus=None,labels=None,loadModel=False,mappingName=None):
     fullCorpusEncoded = []
     labels = fullCorpus["type"]
     if not loadModel:
-        model = api.load("word2vec-google-news-300")  
+        model = api.load("word2vec-google-news-300")
         fullCorpus = fullCorpus.content.apply(lambda x: simple_preprocess(str(x)))
         modelVocab = {word.lower():model[word] for word in model.key_to_index.keys()}
         corpusWords = list(set([x for li in fullCorpus for x in li]))
-        
+
         corpusVocab = defaultdict(list)
         for x in corpusWords:
             try:
                 corpusVocab[x] = modelVocab[x]
             except:
                 pass
-        
+
         corpusWords = [x for x in corpusVocab.values()]
-        dimReducModel = TSNE(n_components=3,n_jobs=-1).fit_transform(np.array(corpusWords)) 
+        dimReducModel = TSNE(n_components=3,n_jobs=-1).fit_transform(np.array(corpusWords))
         corpusVocab = {list(corpusVocab.keys())[count]:list(vec) for count,vec in enumerate(dimReducModel)}
-        
+
         if mappingName != None:
             with open(f"Part2/{mappingName}.pickle","wb") as f:
                 pickle.dump(corpusVocab,f)
@@ -86,7 +86,7 @@ def format(fullCorpus=None,labels=None,loadModel=False,mappingName=None):
         print(f"Lenght of fullcorpus: {len(fullCorpus)}")
         print(f"row: {count}")
         fullCorpusEncoded.append(article)
-    return pd.DataFrame(list(zip(fullCorpusEncoded,labels)),columns=["Article encoded","Labels"])
+    return pd.DataFrame(list(zip(fullCorpusEncoded,labels)),columns=["Article encoded","type"])
 
 
 
