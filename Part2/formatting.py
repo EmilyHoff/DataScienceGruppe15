9bf34b88ae413dfa7e74fc3dda143df173bf8819
BenjaminMJ <rtx245@alumni.ku.dk>
2023-03-28 13:20:03 +0200

    onehotencode

--- Part2/formatting.py
+++ Part2/formatting.py
@@ -20,6 +20,7 @@ from gensim.utils import simple_preprocess
 import gensim.downloader as api
 from gensim import utils
 import nltk
+import Part2.binaryLable as pbl
 
 from sklearn.decomposition import PCA
 from sklearn.manifold import TSNE
@@ -36,6 +37,7 @@ import pandas as pd
 import nltk
 import re
 from gensim.models import Word2Vec
+from sklearn.preprocessing import OneHotEncoder
 from gensim.utils import simple_preprocess
 import gensim.downloader as api
 import pickle
@@ -89,9 +91,41 @@ def format(fullCorpus=None,labels=None,loadModel=False,mappingName=None):
     return pd.DataFrame(list(zip(fullCorpusEncoded,labels)),columns=["Article encoded","type"])
 
 
+def oneHotEncode(df):
 
+    content = df['content']
+    labels = df['type']
+    article = set()
 
+    for words in content:
+        article.update(words.split())
+    vocab = sorted(article)
 
+    # create a dictionary that maps words to their indices in the vocabulary
+    word_to_index = {word: i for i, word in enumerate(vocab)}
+
+    one_hot_vectors = []
+    for words in content:
+        vector = np.zeros(len(vocab))
+        for word in words.split():
+            vector[word_to_index[word]] = 1
+        one_hot_vectors.append(vector)
+
+    enc = OneHotEncoder(dtype=int)
+
+    transformed = enc.fit_transform(one_hot_vectors)
+    encoded = np.array(transformed.toarray())
+
+    # print(len(encoded))
+    # words = []
+    # for i in range(0, len(encoded)):
+    #     df['content'][i] = encoded[i]
+
+    df = pd.DataFrame(zip(encoded, labels), columns=['content', 'type'])
+    df.to_csv('one_hot_encoded.csv')
+    return df
+
+    # vi sigter efter at returnere et dataframe med 2 kolonner, content og type
 
 
 
