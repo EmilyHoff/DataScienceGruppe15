8401d86b5e3c79d659db3889ac56d314ecbd5d84
samuelbc010602 <90405397+samuelbc010602@users.noreply.github.com>
2023-03-29 14:17:35 +0200

    Update formatting.py

--- Part2/formatting.py
+++ Part2/formatting.py
@@ -1,6 +1,9 @@
 from distutils.ccompiler import new_compiler
+from lib2to3.pgen2 import token
 from multiprocessing import reduction
 import sys
+from tokenize import Token
+from unittest.util import _MAX_LENGTH
 import pandas as pd
 from traitlets import default
 import numpy as np
@@ -13,6 +16,7 @@ import os
 
 from sklearn.decomposition import IncrementalPCA    # inital reduction
 from sklearn.manifold import TSNE
+from sklearn.feature_extraction.text import CountVectorizer
 
 from gensim.models import Word2Vec
 from gensim.models import KeyedVectors
@@ -20,15 +24,18 @@ from gensim.utils import simple_preprocess
 import gensim.downloader as api
 from gensim import utils
 import nltk
-import Part2.binaryLable as pbl
 
 from sklearn.decomposition import PCA
 from sklearn.manifold import TSNE
+
+import tensorflow as tf
 
 #import matplotlib.pyplot as plt
 #from matplotlib import pyplot
 import random
 import pickle
+
+import fasttext
 
 nltk.download('punkt')
 
@@ -37,58 +44,63 @@ import pandas as pd
 import nltk
 import re
 from gensim.models import Word2Vec
-from sklearn.preprocessing import OneHotEncoder
 from gensim.utils import simple_preprocess
 import gensim.downloader as api
 import pickle
 
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.preprocessing.text import Tokenizer
+
 nltk.download('punkt')
 
-def format(fullCorpus=None,labels=None,loadModel=False,mappingName=None):
-    corpusVocab = None
-    fullCorpusEncoded = []
-    labels = fullCorpus["type"]
-    if not loadModel:
-        model = api.load("word2vec-google-news-300")
-        fullCorpus = fullCorpus.content.apply(lambda x: simple_preprocess(str(x)))
-        modelVocab = {word.lower():model[word] for word in model.key_to_index.keys()}
-        corpusWords = list(set([x for li in fullCorpus for x in li]))
 
-        corpusVocab = defaultdict(list)
-        for x in corpusWords:
-            try:
-                corpusVocab[x] = modelVocab[x]
-            except:
-                pass
 
-        corpusWords = [x for x in corpusVocab.values()]
-        dimReducModel = TSNE(n_components=1,n_jobs=-1).fit_transform(np.array(corpusWords))
-        corpusVocab = {list(corpusVocab.keys())[count]:list(vec) for count,vec in enumerate(dimReducModel)}
+def format(fullCorpus=None):
+    with open("words.txt","w") as f:
+        for x in fullCorpus["content"]:
+            f.write(x)
+            
+    model = fasttext.train_unsupervised("words.txt",dim=150)
+    embedding_matrix = np.zeros((len(model.words)+1, 150))
 
-        if mappingName != None:
-            with open(f"Part2/{mappingName}.pickle","wb") as f:
-                pickle.dump(corpusVocab,f)
-        else:
-            with open("Part2/encoder.pickle","wb") as f:
-                pickle.dump(corpusVocab,f)
-    else:
-        with open(f"Part2/{mappingName}.pickle", "rb") as f:
-            corpusVocab = pickle.load(f)
-        fullCorpus = fullCorpus.content.apply(lambda x: simple_preprocess(str(x)))
-
-    for count,li in enumerate(fullCorpus):
+    wordMapper = defaultdict(int)
+    wordMapper["0"] = 0
+    embedding_matrix[0] = np.zeros(150)
+    print(f"This is the embedding matrix: {embedding_matrix}")
+    for count,word in enumerate(model.words):
+        wordMapper[word] = count+1
+        embedding_matrix[count+1] = model.get_word_vector(word)
+        
+    encoded = []
+    for y in fullCorpus["content"]:
         article = []
-        print(f"current word {li}")
-        for x in li:
-            try:
-                article.append(corpusVocab[x])
-            except:
-                print(f"passing with word {x}")
-                pass
-        #if len(article) == 0:
-            #print(f"************************Count: {count} The following article has zero words appended {li}************************")
-        fullCorpusEncoded.append(article)
-    return pd.DataFrame(list(zip(fullCorpusEncoded,labels)),columns=["Article encoded","type"])
+        for x in y.split(" "):
+            article.append(wordMapper[x])
+        encoded.append(article)
+    
+    encoded = tf.keras.utils.pad_sequences(encoded,padding="post",truncating="post")  
+
+    return encoded,embedding_matrix,len(model.words)
+
+
+
+def bow(df):
+    # Extract the article content and labels
+    articles = df['content'].tolist()
+    labels = df['type'].tolist()
+
+    # Create a CountVectorizer object to encode the articles
+    vectorizer = CountVectorizer(analyzer="char_wb")
+
+    # Fit the vectorizer on the articles to learn the vocabulary
+    vectorizer.fit(articles)
+    
+    # Transform the articles into bag-of-words vectors
+    encoded_articles = vectorizer.transform(articles)
+    encoded_articles = encoded_articles.toarray().tolist()
+
+    return encoded_articles,labels
+    
 
 
 def oneHotEncode(df):
