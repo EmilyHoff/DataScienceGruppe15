eef10fefad0d228e3804e19be4013867c461343a
EmilyHoff <89389810+EmilyHoff@users.noreply.github.com>
2023-03-30 10:57:19 +0200

    ROC curve

--- Part2/simpleAuthors.py
+++ Part2/simpleAuthors.py
@@ -1,6 +1,7 @@
 import re
 from sklearn.linear_model import LogisticRegression
 from sklearn import metrics
+from sklearn.metrics import roc_curve
 import sklearn.model_selection as sk
 import numpy as np
 import pandas as pd
@@ -56,70 +57,11 @@ def predictByAuthors(trainDf, valDf):
     cmTable.plot()
     plt.show()
 
+    #ROC-curves
+    fpr, tpr, threshold = metrics.roc_curve(y_val,  y_pred)
+    auc = metrics.roc_auc_score(y_val, y_pred)
+    plt.plot(fpr,tpr,label="auc= " + str(auc))
+    plt.legend(loc=4)
+    plt.show()
     return
 
-
-def domianReliability(df):
-    fakeDomains = []
-    relDomains = []
-
-    #compile lists of domians indicating fakeness or reliability
-    types = df['type'].tolist()
-    domains = df['domain'].tolist()
-
-    for x in range(0, len(df)):
-        if types[x] == 0: #fake news
-            fakeDomains.append(domains[x])
-        else:
-            relDomains.append(domains[x])
-
-    fakeDomains = Counter(sorted(fakeDomains))
-    relDomains = Counter(sorted(relDomains))
-
-    #crossreference domains
-    domainScore = []
-
-    for domain in domains:
-        if not(fakeDomains[domain] == 0): #the domain has published fake news
-            if relDomains[domain] == 0: #the domain has only published fake news
-                domainScore.append(0)
-            else:
-                score = fakeDomains[domain]/(relDomains[domain] + fakeDomains[domain])
-                domainScore.append(score)
-        else: # the domain has solely published rel news
-            domainScore.append(1)
-
-    domainScore = pd.DataFrame(domainScore, columns=['domainScore'])
-    return pd.DataFrame(domainScore, columns=['domainScore'])
-
-'''A baseline model for prediction of an articles authencity base on 
-the domain which published it and the number of authors'''
-def predictByMeta(df):
-    #prepare data
-    authorsDF = numberOfAuthors(df)['authors'].to_frame()
-    authorsDF = authorsDF.reset_index()
-
-    domDf = domianReliability(df)
-    domDf = domDf.reset_index()
-
-    x = pd.concat([domDf, authorsDF], axis=1, join='inner')
-    y = df['type'].to_numpy()
-
-    x_train, x_val, y_train, y_val = sk.train_test_split(x, y, test_size=0.2, random_state=0)
-
-    metaMod = LogisticRegression()
-
-    #ensuring correct type within y_data
-    y_train = y_train.astype('int')
-    y_val = y_val.astype('int')
-
-    metaMod = metaMod.fit(x_train, y_train)
-
-    y_pred = metaMod.predict(x_val)
-    y_val = y_val.astype('int')
-
-    print("Accuracy: ", metrics.accuracy_score(y_val, y_pred))
-    print("Recall: ", metrics.recall_score(y_val, y_pred, average='weighted', zero_division=1))
-    print("Precision: ", metrics.recall_score(y_val, y_pred, average='weighted', zero_division=1))
-    print("F1-score: ", metrics.f1_score(y_val, y_pred, average='weighted', zero_division=1))
-    return
