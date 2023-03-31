6e22d8c7086e619584288ed8f7be0cc6b2ed4c0d
samuelbc010602 <90405397+samuelbc010602@users.noreply.github.com>
2023-03-31 11:30:03 +0200

    Update advModels.py

--- Part2/advModels.py
+++ Part2/advModels.py
@@ -44,17 +44,17 @@ def LSTM(encoded,labels,split,vocab_size=None,embedding_matrix=None):
 
     batch_size = 32
 
-    bertModel = Sequential()
-    bertModel.add(tf.keras.layers.Dense(128, activation="relu"))
-    bertModel.add(tf.keras.layers.Reshape((2,64)))
-    bertModel.add(tf.keras.layers.GaussianDropout(0.2))
-    bertModel.add(tf.keras.layers.LSTM(128, activation="tanh"))
-    bertModel.add(tf.keras.layers.GaussianDropout(0.2))
-    bertModel.add(tf.keras.layers.Reshape((2*64,)))
-    bertModel.add(tf.keras.layers.Dense(16, activation='relu'))
-    bertModel.add(tf.keras.layers.Reshape((2, 8)))
-    bertModel.add(tf.keras.layers.LSTM(8))
-    bertModel.add(tf.keras.layers.Dense(1, activation="sigmoid",
+    LSTMModel = Sequential()
+    LSTMModel.add(tf.keras.layers.Dense(128, activation="relu"))
+    LSTMModel.add(tf.keras.layers.Reshape((2,64)))
+    LSTMModel.add(tf.keras.layers.GaussianDropout(0.2))
+    LSTMModel.add(tf.keras.layers.LSTM(128, activation="tanh"))
+    LSTMModel.add(tf.keras.layers.GaussianDropout(0.2))
+    LSTMModel.add(tf.keras.layers.Reshape((2*64,)))
+    LSTMModel.add(tf.keras.layers.Dense(16, activation='relu'))
+    LSTMModel.add(tf.keras.layers.Reshape((2, 8)))
+    LSTMModel.add(tf.keras.layers.LSTM(8))
+    LSTMModel.add(tf.keras.layers.Dense(1, activation="sigmoid",
                                         bias_initializer=tf.keras.initializers.Constant(np.log([sum(labels)/len(labels)]))))
 
     METRICS = [
@@ -69,58 +69,17 @@ def LSTM(encoded,labels,split,vocab_size=None,embedding_matrix=None):
       tf.keras.metrics.AUC(name='prc', curve='PR') # precision-recall curve,
     ]
     
-    bertModel.compile(optimizer='adam',loss='binary_crossentropy',metrics=METRICS)
+    LSTMModel.compile(optimizer='adam',loss='binary_crossentropy',metrics=METRICS)
     
     for i in range(len(trainX) // batch_size):
         print(f"batch: {i}")
         x_batch = trainX[i*batch_size:(i+1)*batch_size]
         y_batch = trainY[i*batch_size:(i+1)*batch_size]
-        bertModel.fit(x_batch, y_batch,class_weight={0:1,1:1})
+        LSTMModel.fit(x_batch, y_batch,class_weight={0:1,1:1})
         
-    metric = bertModel.evaluate(testX,testY)
+    metric = LSTMModel.evaluate(testX,testY)
     return metric
 
-def ensemble(encoded,labels,split):
-    
-    trainX = np.array(encoded[:split])
-    trainY = np.array(labels[:split])
-    
-    scaler = StandardScaler(with_mean=False)
-    X_train_scaled = scaler.fit_transform(trainX)
-    
-    smote = SMOTE()
-    trainX,trainY = smote.fit_resample(X_train_scaled,trainY)
-
-
-    testX = np.array(encoded[split:])
-    testY = np.array(labels[split:])
-        
-    #clf = BaggingClassifier(base_estimator=LogisticRegression(random_state=0,max_iter=15,solver="newton-cg"),
-    #                        n_estimators=25,
-    #                        random_state=0).fit(trainX,trainY)
-    
-    
-    #clf = BaggingClassifier(base_estimator=svm.SVC(),
-    #                       n_estimators=15,
-    #                       random_state=0,verbose=1).fit(trainX,trainY)
-    
-    #clf = BaggingClassifier(base_estimator=KMeans(n_clusters=2, n_init="auto"),
-    #                        n_estimators=25,
-    #                        random_state=0).fit(trainX,trainY)
-    
-    
-    #clf = RandomForestClassifier(n_estimators=25,class_weight="balanced").fit(trainX,trainY)
-    
-    #clf = ExtraTreesClassifier(n_estimators=250, max_depth=5,
-    #                           min_samples_split=2, random_state=0,
-    #                           verbose=4).fit(trainX,trainY)
-    
-    clf = AdaBoostClassifier(base_estimator=LogisticRegression(random_state=0,max_iter=15,solver="newton-cg"),
-                             n_estimators=300).fit(trainX,trainY)
-    
-    y_pred = clf.predict(testX)
-    print(f"This is the ensemble method: {classification_report(testY,y_pred)}")
-    return y_pred
 
 
 
